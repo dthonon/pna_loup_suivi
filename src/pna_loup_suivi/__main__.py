ddda535ae24a94065499fd29948979be34be20fa
Daniel Thonon <d.thonon9@gmail.com>
2022-12-14 00:24:40 +0100

    Implement data summary Fixes #41 - WIP

--- src/pna_loup_suivi/__main__.py
+++ src/pna_loup_suivi/__main__.py
@@ -1,5 +1,16 @@
 """Command-line interface."""
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
+
 import click
+import pandas as pd
+
+from . import _
+from . import __version__
+
+
+logger = logging.getLogger(__name__)
 
 
 @click.command()
@@ -7,6 +18,33 @@ import click
 def main() -> None:
     """Pna_Loup_Suivi."""
 
+    # Create $HOME/tmp directory if it does not exist
+    (Path.home() / "tmp").mkdir(exist_ok=True)
+
+    # create file handler which logs even debug messages
+    fh = TimedRotatingFileHandler(
+        str(Path.home()) + "/tmp/" + __name__ + ".log",
+        when="midnight",
+        interval=1,
+        backupCount=100,
+    )
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s"
+    )
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    logger.setLevel(logging.INFO)
+
+    logger.info(_("Summarize data files"))
+
+    reg = pd.read_csv("data/reg2016.tab", sep="\t")
+    print(reg)
 
 if __name__ == "__main__":
     main(prog_name="pna_loup_suivi")  # pragma: no cover
