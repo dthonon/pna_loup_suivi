03646268781719f2c2eee4e7ca90f72ad76af4f8
Daniel Thonon <d.thonon0@outlook.com>
2026-02-28 23:12:43 +0100

    Upgrade python 3.11

--- noxfile.py
+++ noxfile.py
@@ -24,7 +24,7 @@ except ImportError:
 
 
 package = "pna_loup_suivi"
-python_versions = ["3.10"]
+python_versions = ["3.11"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
